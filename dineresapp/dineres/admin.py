9528502449496561026bf01bb294d2e4f8ec8227
cadicedit2k5@gmail.com <cadicedit2k5@gmail.com>
2026-01-05 00:05:44 +0700

    Custom Admin site

--- dineresapp/dineres/admin.py
+++ dineresapp/dineres/admin.py
@@ -1,30 +1,55 @@
 from django.contrib import admin
 from django.contrib import messages
+from django.contrib.auth.models import Group
 from django.utils.safestring import mark_safe
 from django.utils.translation import ngettext
 
-from .models import Category, Dish, Order, Booking, Review, Table, Ingredient, User, DishDetail, Chef
+from .models import Category, Dish, Order, Booking, Review, Table, Ingredient, User, Chef, Transaction
 
-
-# Register your models here.
-class DineResAppAdmin(admin.AdminSite):
-    site_header = 'Hệ thống nhà hàng trực tuyển'
-
-class CategoryAdmin(admin.ModelAdmin):
-
+# Base Admin
+class BaseAdmin(admin.ModelAdmin):
     readonly_fields = ['hinh_anh']
-
     def hinh_anh(self, obj):
-        if obj and obj.image:  # Kiểm tra xem có ảnh hay không
+        if obj and obj.image:
             return mark_safe(
                 '<img src="{url}" width="120" />'.format(url=obj.image.url)
             )
         return "Chưa có ảnh"
 
+# Inline Admin
+class DishIngredientInline(admin.TabularInline):
+    model = Dish.ingredients.through
+
+# Register your models here.
+class DineResAppAdmin(admin.AdminSite):
+    site_header = 'Hệ thống nhà hàng trực tuyển'
+
+class CategoryAdmin(BaseAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['name']
+
+class IngredientAdmin(BaseAdmin):
+    list_display = ['id', 'name', 'unit', 'active']
+    search_fields = ['name', 'unit']
+    list_filter = ['active']
+    inlines = [DishIngredientInline]
+
+class DishAdmin(BaseAdmin):
+    list_display = ['id', 'name', "chef__user", "category", 'price', "prep_time"]
+    search_fields = ['name', 'category__name', 'chef__name']
+    list_filter = ['active']
+    list_per_page = 20
+    inlines = [DishIngredientInline]
+
+class ReviewAdmin(BaseAdmin):
+    list_display = ['id', 'dish', 'rating', 'comment']
+    search_fields = ['dish__name']
+
 class ChefAdmin(admin.ModelAdmin):
     list_display = ['user__first_name','specialty', 'experience', 'is_verified']
     list_filter = ['is_verified']
     search_fields = ['user__first_name', 'user__last_name', 'specialty']
+    list_per_page = 20
     actions = ['verify_chefs', 'reject_chefs']
 
     @admin.action(description='Duyệt hồ sơ')
@@ -68,15 +93,23 @@ class ChefAdmin(admin.ModelAdmin):
             updated_count,
         ) % updated_count, messages.WARNING)
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', "first_name", "last_name", 'phone', 'email', 'is_active']
+    search_fields = ['username', 'first_name', 'last_name']
+    list_filter = ['is_active']
+    list_per_page = 20
+
+
 admin_site = DineResAppAdmin(name='myadmin')
 
 admin_site.register(Category, CategoryAdmin)
-admin_site.register(Dish)
-admin_site.register(User)
+admin_site.register(Dish, DishAdmin)
+admin_site.register(User, UserAdmin)
 admin_site.register(Chef, ChefAdmin)
-admin_site.register(Order)
+admin_site.register(Ingredient, IngredientAdmin)
 admin_site.register(Booking)
-admin_site.register(Review)
 admin_site.register(Table)
-admin_site.register(Ingredient)
-admin_site.register(DishDetail)
+admin_site.register(Order)
+admin_site.register(Transaction)
+admin_site.register(Review, ReviewAdmin)
+admin_site.register(Group)
