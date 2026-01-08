51cefd5fd57adeb5db431d29a4e5976787cc4cbf
cadicedit2k5@gmail.com <cadicedit2k5@gmail.com>
2026-01-08 15:19:48 +0700

    Sửa lại chức năng thanh toán. Đã có thể hoạt động được với app ZaloPay Test

--- dineresapp/dineres/views/order_views.py
+++ dineresapp/dineres/views/order_views.py
@@ -1,12 +1,13 @@
 from rest_framework import viewsets, generics, permissions, status
 from rest_framework.decorators import action
+from rest_framework.generics import RetrieveAPIView
 from rest_framework.response import Response
 
 from dineres.models import Order
 from dineres.serializers.order_serializers import OrderSerializer
 
 
-class OrderViewSet(viewsets.ViewSet, generics.CreateAPIView, generics.ListAPIView, generics.UpdateAPIView):
+class OrderViewSet(viewsets.ViewSet, generics.CreateAPIView, generics.ListAPIView, generics.UpdateAPIView, RetrieveAPIView):
     # Lay ten khach hang dung 1 lan
     # Lay order details dung 1 lan va hien thi lun dish trong order do
     queryset = Order.objects.filter(active=True).select_related().select_related('customer').prefetch_related('details__dish')
--- dineresmobileapp/screens/Cart/Payment.js
+++ dineresmobileapp/screens/Cart/Payment.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { View, StyleSheet, ScrollView, Alert, TouchableOpacity } from 'react-native';
 import { Text, RadioButton, Icon, Surface } from 'react-native-paper';
 import { useNavigation, useRoute } from '@react-navigation/native';
@@ -6,9 +6,10 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { authApis, endpoints } from '../../utils/Apis';
 import MyStyles from '../../styles/MyStyles';
 import GoBack from '../../components/Layout/GoBack';
-import { Linking } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import MyButton from '../../components/Layout/MyButton';
+import * as Linking from "expo-linking"
+import { ViewModeContext } from '../../utils/contexts/MyContexts';
 
 const Payment = () => {
     const route = useRoute();
@@ -16,21 +17,17 @@ const Payment = () => {
     const { orderId, totalAmount } = route.params || {};
     const [paymentMethod, setPaymentMethod] = useState('cash');
     const [loading, setLoading] = useState(false);
+    const [isCustomerView] = useContext(ViewModeContext);
+
+    const redirectUrl = Linking.createURL('payment-result')
 
     const methods = [
-        { 
-            value: 'cash', 
-            label: 'Thanh toán tiền mặt', 
-            icon: 'cash', 
-            color: '#4CAF50',
-            desc: 'Thanh toán trực tiếp cho shipper'
-        },
         { 
             value: 'momo', 
             label: 'Ví MoMo', 
             icon: 'wallet',
             color: '#A50064',
-            desc: 'Thanh toán qua ứng dụng MoMo'
+            desc: 'Thanh toán MoMo'
         },
         { 
             value: 'zlpay', 
@@ -55,6 +52,61 @@ const Payment = () => {
         // },
     ];
 
+    if (!isCustomerView) {
+        methods.unshift({ 
+            value: 'cash', 
+            label: 'Thanh toán tiền mặt', 
+            icon: 'cash', 
+            color: '#4CAF50',
+            desc: 'Thanh toán trực tiếp'
+        },)
+    }
+
+    useEffect(() => {
+        const handleDeepLink = (event) => {
+            let url = event.url;
+            console.log("App quay lại từ link:", url);
+            
+            if (url && url.includes('payment-result')) {
+                checkPaymentStatus(); 
+            }
+        };
+
+        const subscription = Linking.addEventListener('url', handleDeepLink);
+        
+        Linking.getInitialURL().then((url) => {
+            if (url && url.includes('payment-result')) {
+                checkPaymentStatus();
+            }
+        });
+
+        return () => {
+            subscription.remove();
+        };
+    }, []);
+
+    const checkPaymentStatus = async () => {
+        try {
+            setLoading(true);
+            const token = await AsyncStorage.getItem("access-token");
+            const res = await authApis(token).get(endpoints['order-detail'](orderId));
+            
+            console.log(res.data);
+
+            if (res.data.status === 'paid') {
+                Alert.alert("Thành công", "Thanh toán thành công!", [
+                    { text: "Về trang chủ", onPress: () => nav.navigate("Home") }
+                ]);
+            } else {
+                Alert.alert("Thông báo", "Giao dịch đang xử lý hoặc chưa hoàn tất.");
+            }
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
     const handlePayment = async () => {
         if (!orderId) {
             Alert.alert("Lỗi", "Không tìm thấy thông tin đơn hàng");
@@ -68,18 +120,22 @@ const Payment = () => {
             const res = await authApis(token).post(endpoints['payments'], {
                 "order_id": orderId,
                 "payment_method": paymentMethod,
+                "redirect_url": redirectUrl
             });
 
-            if (res.status === 201 || res.status === 200) {
-                if (res.data.payUrl) {
-                        Linking.openURL(res.data.payUrl);
-                        Alert.alert("Chuyển hướng", "Đang chuyển sang cổng thanh toán...");
-                    } else {
-                         Alert.alert("Thông báo", "Yêu cầu thanh toán đã được gửi.");
-                    }
+            if (paymentMethod === 'cash') {
+                Alert.alert("Thành công", "Thanh toán thành công!", [
+                    { text: "Về trang chủ", onPress: () => nav.navigate("Home") }
+                ]);
             }
+
+            const data =res.data;
+            if (data.payUrl) {
+                await Linking.openURL(data.payUrl);
+            }
+
         } catch (error) {
-            console.error(error.response.data.message);
+            console.error(error.response.data);
             Alert.alert("Lỗi", "Thanh toán thất bại. Vui lòng thử lại.");
         } finally {
             setLoading(false);
